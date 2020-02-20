4eb92aa29f822b96ba1e923ad487192ab4b1fe05
Tarek Ismail <ti81doga@studserv.uni-leipzig.de>
2020-02-20 12:57:12 +0100

    Update

--- Angular/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ Angular/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('A test Recip','this is a simply test','https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_1160,h_1450/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103'),
+    new Recipe('A test Recip','this is a simply test','https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fit,w_1160,h_1450/k%2FPhoto%2FRecipes%2F2019-09-how-to-shrimp-alfredo%2FHT-Shrimp-Alfredo_103')
+
+  ]; 
 
   constructor() { }
 
